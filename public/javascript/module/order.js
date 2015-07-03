d9f0727e8924d90e83ad10570a13e16cc030584a
yagamai <zodiake.wang@ihotkeys.com>
2015-07-03 18:37:01 +0800

    change

--- public/javascript/module/order.js
+++ public/javascript/module/order.js
@@ -1,7 +1,9 @@
 var order = angular.module('Order', []);
 
 order.service('OrderService', ['$http', function($http) {
-
+    this.findAll = function(option) {
+        return $http.get('/admin/order/');
+    }
 }]);
 
 order.controller('OrderController', ['$scope', function($scope) {
