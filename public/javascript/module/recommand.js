16976b0ba0c317814fc3e64569cf1eba3b31e006
angelaning <1165376108@qq.com>
2015-07-10 13:40:00 +0800

    update recommand.html

--- public/javascript/module/recommand.js
+++ public/javascript/module/recommand.js
@@ -1,5 +1,15 @@
 var recommand = angular.module('Recommand', []);
 
 recommand.controller('RecommandController', ['$scope', function($scope) {
-
+    $scope.items = [{
+        recommand_id: '1',
+        con: 'asdsadfasfdasgfdasgsadg',
+        status: '已读',
+        date: '2015-07-09'
+    }, {
+        recommand_id: '2',
+        con: 'ddddddddddddddddddddd',
+        status: '已读',
+        date: '2015-07-09'
+    }]
 }]);
