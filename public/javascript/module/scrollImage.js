00a3712d31c34926e67ca39220c2080b49732b84
yagamai <zodiake.wang@ihotkeys.com>
2015-07-20 13:37:50 +0800

    image upload feature

--- public/javascript/module/scrollImage.js
+++ public/javascript/module/scrollImage.js
@@ -1,6 +1,27 @@
 var scrollImage = angular.module('ScrollImage', []);
 
 
-scrollImage.controller('ScrollController', ['$scope', function($scope) {
-
+scrollImage.controller('ScrollController', ['$scope', 'Upload', function ($scope, Upload) {
+    $scope.upload = function (files) {
+        if (files && files.length) {
+            for (var i = 0; i < files.length; i++) {
+                var file = files[i];
+                console.log(file.url);
+                Upload.upload({
+                    url: 'upload/url',
+                    fields: {
+                        'username': $scope.username
+                    },
+                    file: file
+                }).progress(function (evt) {
+                    var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+                    console.log('progress: ' + progressPercentage + '% ' + evt.config.file.name);
+                }).success(function (data, status, headers, config) {
+                    console.log('file ' + config.file.name + 'uploaded. Response: ' + data);
+                }).error(function (data, status, headers, config) {
+                    console.log('error status: ' + status);
+                })
+            }
+        }
+    };
 }]);
