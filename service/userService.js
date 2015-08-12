5eb511d74af49377f61ade41d0f79f8b8f6e114b
yagamai <zodiake.wang@ihotkeys.com>
2015-08-12 14:13:51 +0800

    r

--- service/userService.js
+++ service/userService.js
@@ -82,17 +82,3 @@ var service = {
 };
 
 module.exports = service;
-
---versionservice
-/*jslint node: true */
-    'use strict';
-var pool = require('../utils/pool');
-
-var service = {
-    findOne: function () {
-        var sql = 'select * from app_version';
-        return pool.query(sql, []);
-    }
-}
-
-module.exports = service;
